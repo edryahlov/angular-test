1a8710bdcd44ac4ea19b71f1b10969a8b3c9968f
Eugene Dryahlov <egenius333@gmail.com>
2017-12-23 11:59:10 +0400

    final

--- src/app/breed/breed.component.ts
+++ src/app/breed/breed.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from "@angular/router"
 import { DataService } from "../data.service"
-import {isArray} from "util";
 
 @Component({
   selector: 'app-breed',
   templateUrl: './breed.component.html',
-  styleUrls: ['./breed.component.css'],
+  styleUrls: ['./breed.component.scss'],
 })
+
 export class BreedComponent implements OnInit, OnDestroy {
 
     private routeSub:any;
@@ -22,59 +22,50 @@ export class BreedComponent implements OnInit, OnDestroy {
         private dataService:DataService,
     ) { }
 
+    //при выборе породы в списке
     breedSelect(breed) {
-        // /api/breed/{breed name}/{sub-breed name}/images/random
-        if (breed.match(/-/g)) {breed = breed.replace(/(\w+)-(\w+)/g,'$1\/$2')}
-        console.log(breed)
+        if (breed.match(/-/g)) {breed = breed.replace(/(\w+)-(\w+)/g,'$1\/$2');} //переделывам ссылку - разные запросы на породу и под-породы
         this.dataService.get('https://dog.ceo/api/breed/'+breed+'/images/random').subscribe(data => {
             this.img = data['message'];
             this.state = 'founded';
             this.breedName = data['message'].replace(/(https:\/\/dog.ceo\/api\/img\/)(.*)\/(.*)/g,'$2');
-            console.log('onSelect: ',breed)
         });
     }
 
   ngOnInit() {
       this.routeSub = this.route.params.subscribe(params => {
-          console.log(params);
 
-          if (params.slug) {
-              this.slug = params.slug.toUpperCase();
-              // this.state = 'xxxx'
-          } else {
-              this.state = 'default'
-          }
+          if (params.slug) this.slug = params.slug.toUpperCase();
+          else this.state = 'default'
+
+          //делаем запрос на список и в селект его
           this.dataService.get('https://dog.ceo/api/breeds/list/all').subscribe(data => {
-              // this.list = Object.keys(data['message']).map(key=>key);
-
-              // с подпородами не разобрался как запрос делать
               for (let key in data['message']) {
                   this.list.push(key);
+                  //если есть под-породы - их тоже добавляем
                   if (data['message'][key].length > 0) data['message'][key].map(d=>this.list.push(key+'-'+d));
               }
-              console.log(this.list)
           });
 
           let path:string;
-          if (this.state === 'default') {path = 'https://dog.ceo/api/breeds/image/random';}
-          else {path = 'https://dog.ceo/api/breed/' + params.slug + '/images/random';}
+          let pathDetail:string;
 
-          // console.log(params.slug)
+          if (this.state === 'default') {path = 'https://dog.ceo/api/breeds/image/random';} //рандомная картинка если ничего не выбрано и нет слага
+          else {
+              //тут модифицируем запрос - если есть под-порода
+              pathDetail = params.slug;
+              if (pathDetail.match(/-/g)) {pathDetail = pathDetail.replace(/(\w+)-(\w+)/g,'$1\/$2')}
+              path = 'https://dog.ceo/api/breed/' + pathDetail + '/images/random';
+          }
 
           this.dataService.get(path).subscribe(data => {
-              // console.log('message>>',data['message']);
-              if (data['status'] !== 'error') {
+              if (data['status'] !== 'error') { //все хорошо
                   this.img = data['message'];
                   this.breedName = data['message'].replace(/(https:\/\/dog.ceo\/api\/img\/)(.*)\/(.*)/g,'$2');
-                  // console.log(this.breedName)
                   this.state = 'founded';
-                  // this.breedGet = params;
-                  console.log(data)
-              } else {
+              } else { //все не оч)
                   this.state = 'not found';
-                  console.log(data)
               }
-              // console.log('inside ',found)
           });
 
       })
